863b5c37cb2a4da06b915bb84eb7c9fca162208d
Viktor <palchinskij19@gmail.com>
2024-08-07 15:20:21 +0300

    06.08.24 - Builder pattern example comment added

--- Builder/exercise/index.js
+++ Builder/exercise/index.js
@@ -1,3 +1,20 @@
+// You are asked to implement the Builder design pattern for rendering simple chunks of code.
+
+// Sample use of the builder you are asked to create:
+
+// let cb = new CodeBuilder('Person');
+// cb.addField('name').addField('age');
+// console.log(cb.toString());
+// The expected output of the above code is:
+
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+// Please observe the same placement of spaces and indentation.
+
 class Field {
     constructor(name) {
         this.name = name;
@@ -28,7 +45,6 @@ class Class {
             }
             buffer.push('  }\n');
         }
-
         buffer.push('}');
         return buffer.join('');
     }
