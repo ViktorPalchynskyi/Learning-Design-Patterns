3998d907e231ee19712db0341db4e58e2a13288a
Viktor <palchinskij19@gmail.com>
2024-08-11 19:50:39 +0300

    10.08.24 - understanding property  Proxy pattern

--- Structural_Patterns/Proxy/index.js
+++ Structural_Patterns/Proxy/index.js
@@ -14,4 +14,42 @@ class Percentage {
 
 const fivePersenct = new Percentage(5);
 
-console.log(`5% of 50 is ${50 * fivePersenct}`);
+// console.log(`5% of 50 is ${50 * fivePersenct}`);
+
+class Property {
+    constructor(value, name = '') {
+        this._value = value;
+        this.name = name;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(newValue) {
+        if (this._value === newValue) {
+            return;
+        }
+
+        console.log(`Assinging ${newValue} to ${this.name}`);
+        this._value = newValue;
+    }
+}
+
+class Creature {
+    constructor() {
+        this._agility = new Property(10, 'agility');
+    }
+
+    get agility() {
+        return this._agility;
+    }
+
+    set agility(value) {
+        this._agility.value = value;
+    }
+}
+
+const c = new Creature();
+c.agility = 22;
+c.agility = 10;
