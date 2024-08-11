d69ae372815f005c9d4fdbb290d31956fa35aa56
Viktor <palchinskij19@gmail.com>
2024-08-11 20:03:26 +0300

    10.08.24 - understanding vertual Proxy pattern

--- Structural_Patterns/Proxy/index.js
+++ Structural_Patterns/Proxy/index.js
@@ -84,3 +84,37 @@ class Driver {
 const car = new CarProxy(new Driver(17));
 
 car.drive();
+
+class LazyImage {
+    constructor(url) {
+        this.url = url;
+    }
+
+    draw() {
+        if (!this.image) {
+            this.image = new Image(this.url);
+        }
+
+        this.image.draw();
+    }
+}
+
+class Image {
+    constructor(url) {
+        this.url = url;
+        console.log(`Loading image from ${url}`);
+    }
+
+    draw() {
+        console.log(`Drawing image from ${this.url}`);
+    }
+}
+
+function drawImage(img) {
+    console.log('About to draw the image.');
+    img.draw();
+    console.log('Done drawing the image');
+}
+
+const img = new LazyImage('https://random.com/image');
+drawImage(img);
