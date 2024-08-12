fe9ef268cdbb41f8662a7de12a612489e25b7e07
Viktor <palchinskij19@gmail.com>
2024-08-12 12:50:33 +0300

    10.08.24 -  Chanine of responsibility pattern exercise

--- Behavioral_Patterns/Chain_of_responsibility/index.js
+++ Behavioral_Patterns/Chain_of_responsibility/index.js
@@ -1,64 +1,137 @@
+class Event {
+    constructor() {
+        this.handlers = new Map();
+        this.count = 0;
+    }
+
+    subscribe(handler) {
+        this.handlers.set(++this.count, handler);
+        return this.count;
+    }
+
+    unsubscribe(idx) {
+        this.handlers.delete(idx);
+    }
+
+    fire(sender, args) {
+        this.handlers.forEach(function (v, k) {
+            v(sender, args);
+        });
+    }
+}
+
+let WhatToQuery = Object.freeze({
+    attack: 1,
+    defense: 2,
+});
+
+class Query {
+    constructor(creatureName, whatToQuery, value) {
+        this.creatureName = creatureName;
+        this.whatToQuery = whatToQuery;
+        this.value = value;
+    }
+}
+
+class Game {
+    constructor() {
+        this.queries = new Event();
+    }
+
+    performQuery(sender, query) {
+        this.queries.fire(sender, query);
+    }
+}
+
 class Creature {
-    constructor(name, attack, defense) {
+    constructor(game, name, attack, defense) {
+        this.game = game;
         this.name = name;
-        this.attack = attack;
-        this.defense = defense;
+        this.initial_attack = attack;
+        this.initial_defense = defense;
+    }
+
+    get attack() {
+        let q = new Query(
+            this.name,
+            WhatToQuery.attack,
+            this.initial_attack
+        );
+        this.game.performQuery(this, q);
+        return q.value;
+    }
+
+    get defense() {
+        let q = new Query(
+            this.name,
+            WhatToQuery.defense,
+            this.initial_defense
+        );
+        this.game.performQuery(this, q);
+        return q.value;
     }
 
     toString() {
-        return `${this.name} (${this.attack}/${this.defense})`;
+        return `${this.name}: (${this.attack}/${this.defense})`;
     }
 }
 
 class CreatureModifier {
-    constructor(creature) {
+    constructor(game, creature) {
+        this.game = game;
         this.creature = creature;
-        this.next = null;
+        this.token = game.queries.subscribe(this.handle.bind(this));
     }
 
-    add(modifier) {
-        if (this.next) {
-            this.next.add(modifier);
-        } else {
-            this.next = modifier;
-        }
+    handle(sender, query) {
+        // implement in inheritors
     }
 
-    handle() {
-        if (this.next) {
-            this.next.handle();
-        }
+    dispose() {
+        game.queries.unsubscribe(this.token);
     }
 }
 
 class DoubleAttackModifier extends CreatureModifier {
-    constructor(creature) {
-        super(creature);
+    constructor(game, creature) {
+        super(game, creature);
     }
 
-    handle() {
-        console.log(`Doubling ${this.creature.name}'s atack`);
-        this.creature.attack *= 2;
-        super.handle();
+    handle(sender, query) {
+        if (
+            query.creatureName === this.creature.name &&
+            query.whatToQuery === WhatToQuery.attack
+        ) {
+            query.value *= 2;
+        }
     }
 }
 
 class IncreaseDefenseModifier extends CreatureModifier {
-    constructor(creature) {
-        super(creature);
+    constructor(game, creature) {
+        super(game, creature);
     }
 
-    handle() {
-        console.log(`Doubling ${this.creature.name}'s defense`);
-        this.creature.defense *= 2;
-        super.handle();
+    handle(sender, query) {
+        if (
+            query.creatureName === this.creature.name &&
+            query.whatToQuery === WhatToQuery.defense
+        ) {
+            query.value += 2;
+        }
     }
 }
 
-const goblin = new Creature('Goblin', 1, 1);
-const root = new CreatureModifier(goblin);
-root.add(new DoubleAttackModifier(goblin));
-root.add(new DoubleAttackModifier(goblin));
-root.add(new IncreaseDefenseModifier(goblin));
-root.handle();
+let game = new Game();
+let goblin = new Creature(game, 'Strong Goblin', 2, 2);
+console.log(goblin.toString());
+
+let dam = new DoubleAttackModifier(game, goblin);
+console.log(goblin.toString());
+
+let idm = new IncreaseDefenseModifier(game, goblin);
+console.log(goblin.toString());
+idm.dispose();
+
+dam.dispose();
 console.log(goblin.toString());
