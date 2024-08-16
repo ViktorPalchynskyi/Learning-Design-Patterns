588db9ebe7ba2cac4b2030a265f0d4c36f06daf9
Viktor <palchinskij19@gmail.com>
2024-08-16 17:00:49 +0300

    16.08.24 - understanding  property dependencies with Observer pattern

--- Behavioral_Patterns/Observer/index.js
+++ Behavioral_Patterns/Observer/index.js
@@ -19,29 +19,116 @@ class Event {
     }
 }
 
-class FallsIllArgs {
-    constructor(address) {
-        this.address = address;
+// class FallsIllArgs {
+//     constructor(address) {
+//         this.address = address;
+//     }
+// }
+
+// class Person {
+//     constructor(address) {
+//         this.address = address;
+//         this.fallsIll = new Event();
+//     }
+
+//     catchCold() {
+//         this.fallsIll.fire(this, new FallsIllArgs(this.address));
+//     }
+// }
+
+class PropertyChangedArgs {
+    constructor(name, newValue) {
+        this.name = name;
+        this.newValue = newValue;
     }
 }
 
 class Person {
-    constructor(address) {
-        this.address = address;
-        this.fallsIll = new Event();
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+        this.propertyChanged = new Event();
     }
 
-    catchCold() {
-        this.fallsIll.fire(this, new FallsIllArgs(this.address));
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        if (!value || this._age === value) {
+            return;
+        }
+
+        const oldCanVote = this.canVote;
+
+        this._age = value;
+        this.propertyChanged.fire(
+            this,
+            new PropertyChangedArgs('age', value)
+        );
+
+        if (oldCanVote !== this.canVote) {
+            this._age = value;
+            this.propertyChanged.fire(
+                this,
+                new PropertyChangedArgs('canVote', value)
+            );
+        }
+    }
+
+    get canVote() {
+        return this._age >= 16;
     }
 }
 
-const p = new Person('Divenskaya 5');
-const sub = p.fallsIll.subscribe((s, a) => {
-    console.log(`A doctor has been called to ${a.address}`);
-});
-p.catchCold();
-p.catchCold();
+class VotingChecker {
+    constructor(person) {
+        this.person = person;
+        this.person.propertyChanged.subscribe(this.votingChanged.bind(this));
+    }
 
-p.fallsIll.unsubscribe(sub);
-p.catchCold();
+    votingChanged(sender, args) {
+        if (sender === this.person && args.name === 'canVote') {
+            console.log(`Voting status changed to ${args.newValue}`);
+        }
+    }
+}
+
+// class RegistrationChecker {
+//     constructor(person) {
+//         this.person = person;
+//         this.token = person.propertyChanged.subscribe(
+//             this.ageChanged.bind(this)
+//         );
+//     }
+
+//     ageChanged(sender, args) {
+//         if (sender === this.person && args.name === 'age') {
+//             if (args.newValue < 13) {
+//                 console.log(`Sorry, you are still to young`);
+//             } else {
+//                 console.log(`Okey, you can register`);
+//                 sender.propertyChanged.unsubscribe(this.token);
+//             }
+//         }
+//     }
+// }
+
+// const p = new Person('Divenskaya 5');
+// const sub = p.fallsIll.subscribe((s, a) => {
+//     console.log(`A doctor has been called to ${a.address}`);
+// });
+// p.catchCold();
+// p.catchCold();
+
+// p.fallsIll.unsubscribe(sub);
+// p.catchCold();
+
+const p = new Person('John');
+const checker = new VotingChecker(p);
+// const c = new RegistrationChecker(p);
+
+for (let i = 10; i < 20; ++i) {
+    console.log(`Changing age to ${i}`);
+    p.age = i;
+}
