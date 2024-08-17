5f1b061e5f40baf9cc3157732d087c6722f6bd5d
Viktor <palchinskij19@gmail.com>
2024-08-17 15:47:00 +0300

    17.08.24 -Flyweight pattern with learn.js

---
+++ Structural_Patterns/Flyweight/learn_js_course/index.js
@@ -0,0 +1,24 @@
+const treeBehavior1 = {};
+const treeBehavior2 = {};
+const treeBehavior3 = {};
+
+const tree1 = {
+    x: 0,
+    y: 0,
+    behavior: treeBehavior1,
+};
+const tree2 = {
+    x: 22,
+    y: 42,
+    behavior: treeBehavior1,
+};
+const tree3 = {
+    x: 51,
+    y: 24,
+    behavior: treeBehavior1,
+};
+const tree4 = {
+    x: 55,
+    y: 11,
+    behavior: treeBehavior1,
+};
